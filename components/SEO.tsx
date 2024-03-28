03ad373f4bb4e658dc8f862dafa0db390789597a
Erdenebulgan Sainkhishig <sunerdenebulgan@gmail.com>
2024-03-28 21:07:59 +0000

    add site analytics

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
 type Props = {
-    title: string
-    meta_name: string
-    content: string
-}
+  title: string;
+  meta_name: string;
+  content: string;
+};
 
 const SEO: React.FC<Props> = ({ title, meta_name, content }) => {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name={meta_name} content={content} />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-    )
-}
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name={meta_name} content={content} />
+      <link rel="icon" href="/favicon.ico" />
+      <script
+        defer
+        src="https://analytics.us.umami.is/script.js"
+        data-website-id="332e042f-27ed-4850-a4fe-cad8619322ed"
+      ></script>
+    </Head>
+  );
+};
 
 export default SEO;
